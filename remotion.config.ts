a0794dffdd576c40be382b8c01ea05e993ae7dae
Bavos <87132712+Bavos@users.noreply.github.com>
2026-05-18 17:27:34 -0300

    Update remotion.config.ts

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,5 +1,5 @@
-import {Config} from 'remotion';
+import {Config} from "@remotion/cli/config";
 
-Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
-Config.setCodec('h264');
+Config.setVideoImageFormat("jpeg");
+Config.setChromiumOpenGlRenderer("angle");
